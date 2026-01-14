aab0c82f719c7f0fa2eaca73b7157b89e1e88c74
MaksimObozniy <125642497+MaksimObozniy@users.noreply.github.com>
2026-01-14 16:52:18 +0100

    Echobot for VK bot

--- vk_bot.py
+++ vk_bot.py
@@ -4,21 +4,32 @@ from dotenv import load_dotenv
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
+
+def send_message(vk_api_client, user_id: int, text: str) -> None:
+    vk_api_client.messages.send(
+        user_id=user_id,
+        message=text,
+        random_id=0,
+    )
+
+
 def main():
     load_dotenv()
     token = os.environ["VK_GROUP_TOKEN"]
     
     vk_session = vk_api.VkApi(token=token)
+    vk_api_client = vk_session.get_api()
     longpool = VkLongPoll(vk_session)
     
     print("VK Listener Started")
     
     for event in longpool.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            print("Новое сообщение:")
-            print("От:", event.user_id)
-            print("Текст:", event.text)
-            print("-" * 30)
+            incoming_text = event.text
+            user_id = event.user_id
+            
+            send_message(vk_api_client, user_id=user_id, text=incoming_text)
+            
             
 if __name__ == "__main__":
     main()
